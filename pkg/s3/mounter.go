aa04bcbbaf84ea4b3d498d69416df9ca98d3fb3d
HunterChen <1480811935cyt@gmail.com>
2022-08-11 17:03:06 +0800

    add noxmlns

--- pkg/s3/mounter.go
+++ pkg/s3/mounter.go
@@ -56,6 +56,7 @@ func (s3fs *s3fsMounter) Mount(source string, target string) error {
 		"-o", fmt.Sprintf("url=%s", s3fs.url),
 		"-o", fmt.Sprintf("endpoint=%s", s3fs.region),
 		"-o", "allow_other",
+		"-o", "noxmlns",
 		"-o", "mp_umask=000",
 	}
 	return fuseMount(s3fsCmd, args)
